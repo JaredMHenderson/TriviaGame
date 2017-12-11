3db5daf9c35f5b577ea508df53ac2b0db0205910
Jared Henderson <jaredmhenderson@icloud.com>
2017-12-10 19:02:47 -0700

    added sounds

--- assets/logic.js
+++ assets/logic.js
@@ -18,22 +18,34 @@ var questions = [{
     question: 'At Jims house party, what does Pam find in Jims room that he later gives her as a gift?',
     answers: ['A Piggy Bank', 'A Sweater', 'A Yearbook Photo', 'A Baseball Trophy'],
     answer: 'A Yearbook Photo',
-    image: './assets/images/jimpic.png'
+    image: './assets/images/jimpic.png',
+    wrongImage: './assets/images/dwightwrong1.gif',
+    correctsound: './assets/sounds/Hero.mp3',
+    wrongsound: './assets/sounds/Creedwebsite.mp3'
     }, {
     question: 'What food item caused the unintentional fire to start in the office building?',
     answers: ['Kevins homemade chilli', 'Ryans cheesy pita', 'Creeds Mung Beans', 'Michaels Chicken Pot Pie'],
     answer: 'Ryans cheesy pita',
-    image: './assets/images/ryan.jpg'
+    image: './assets/images/ryan.jpg',
+    wrongImage: './assets/images/kevinwrong.jpg',
+    correctsound: './assets/sounds/Thankyouvery muchSir.mp3',
+    wrongsound: './assets/sounds/Whatisyourproblem.mp3'
     }, {
     question: 'What animal did the employees of the office hold a funeral for in the parking lot?',
     answers: ['A Bird', 'A Racoon', 'A Turtle', 'A Bat'],
     answer: 'A Bird',
-    image: './assets/images/birdfuneral.jpg'
+    image: './assets/images/birdfuneral.jpg',
+    wrongImage: './assets/images/flyaway.gif',
+    correctsound: './assets/sounds/Neverpegged.mp3',
+    wrongsound: './assets/sounds/Blackbear.mp3'
     }, {
     question: 'Jim sends Dwight on a quest for the Holy Grail, the instructsion are called what?',
     answers: ['Threat Level Midnight', 'The Dunder Code', 'Dunder Mifflin Infinity', 'Second Life'],
     answer: 'The Dunder Code',
-    image: './assets/images/dwightpic.jpg'
+    image: './assets/images/dwightpic.jpg',
+    wrongImage: './assets/images/dwightwig.gif',
+    correctsound: './assets/sounds/Everythingiscool.mp3',
+    wrongsound: './assets/sounds/I Will Destroy You.mp3'
     }
     ];
 
@@ -109,7 +121,11 @@ var game = {
         clearInterval(timer);
         game.correct++;
         $('#subwrapper').html(`<h3>${'That is Correct!'}</h3`);
-         $('#subwrapper').append(`<img src = "${questions[game.currentQuestion].image}">`);
+        $('#subwrapper').append(`<img src = "${questions[game.currentQuestion].image}" style="width: 65%; height: 65%;">`);
+        $('#subwrapper').append(`
+            <audio controls autoplay style="display: none;">
+            <source src="${questions[game.currentQuestion].correctsound}" type="audio/ogg">
+            `);
         if(game.currentQuestion == questions.length-1){
             setTimeout(game.results, 5000);
         } else {
@@ -123,6 +139,11 @@ var game = {
         clearInterval(timer);
         game.incorrect++;
         $('#subwrapper').html(`<h3>${'That is Incorrect!'}</h3`)
+        $('#subwrapper').append(`<img src = "${questions[game.currentQuestion].wrongImage}" style="width: 65%; height: 65%;">`);
+        $('#subwrapper').append(`
+            <audio controls autoplay style="display: none;">
+            <source src="${questions[game.currentQuestion].wrongsound}" type="audio/ogg">
+            `);
         if(game.currentQuestion == questions.length-1){
             setTimeout(game.results, 3000);
         } else {
